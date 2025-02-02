259a9c6b393b4d25384556e2feb1ddcd86d0a6e8
Navneet Singh Gill <navneet105gill@gmail.com>
2025-02-01 23:37:39 -0500

    Initialise

---
+++ UserInterfaceSwiftNavneet/UserInterfaceSwiftNavneetApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterfaceSwiftNavneetApp.swift
+//  UserInterfaceSwiftNavneet
+//
+//  Created by Navneet Singh Gill on 2025-02-01.
+//
+
+import SwiftUI
+
+@main
+struct UserInterfaceSwiftNavneetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
